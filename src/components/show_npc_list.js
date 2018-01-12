e1349531c3e290d74dfc39ce206a8f6168d7ed10
permalost <permalost@gmail.com>
2018-01-11 21:34:00 -0700

    Trying out Material-UI GridList. Also, hardcoded items in blog

--- src/components/show_npc_list.js
+++ src/components/show_npc_list.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { GridList, GridTile } from 'material-ui/GridList';
 
-import Npc from './show_npc';
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 800,
+    height: 1000,
+    overflowY: 'auto',
+  }
+}
 
 class NpcList extends Component {
   renderList() {
     return (
-      this.props.npcs.map((npc) => <Npc npc={npc} />)
+      this.props.npcs.map((npc) =>
+        <GridTile
+          key={npc.thumbnail}
+          title={npc.name}
+          subtitle={npc.associations}
+          >
+          <img src={npc.thumbnail} />
+        </GridTile>)
     );
   }
   render() {
     return (
-      <div>
+      <div style={styles.root}>
+        <GridList
+          style={styles.gridList}
+          cellHeight={300}
+        >
           {this.renderList()}
+        </GridList>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -12,6 +13,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <NpcList />
+    <MuiThemeProvider>
+      <NpcList />
+    </MuiThemeProvider>
   </Provider>
   , document.querySelector('.container'));
