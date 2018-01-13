b42ee415256950a50827d04edd9d6b0feafea499
permalost <permalost@gmail.com>
2018-01-12 19:56:55 -0700

    Connected reducer, updated material-ui, gridlist working

--- src/components/show_npc_list.js
+++ src/components/show_npc_list.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { GridList, GridTile } from 'material-ui/GridList';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { GridList, GridListTile, GridListTileBar } from 'material-ui/GridList';
 
-const styles = {
+const cellHeight = 180;
+
+const styles = theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: 800,
-    height: 1000,
-    overflowY: 'auto',
+    width: 750,
+    height: 500,
+  },
+  img: {
+    height: {cellHeight},
+    width: 125,
+    mode: "fit"
   }
-}
+});
 
 class NpcList extends Component {
+
   renderList() {
+    const { classes } = this.props;
+
     return (
       this.props.npcs.map((npc) =>
-        <GridTile
-          key={npc.thumbnail}
-          title={npc.name}
-          subtitle={npc.associations}
-          >
-          <img src={npc.thumbnail} />
-        </GridTile>)
+        <GridListTile key={npc.thumbnail}>
+          <img src={npc.thumbnail} alt={npc.name} className={classes.img} />
+          <GridListTileBar
+            title={npc.name}
+            subtitle={npc.associations}
+           />
+        </GridListTile>)
     );
   }
   render() {
+    const { classes } = this.props;
+
     return (
-      <div style={styles.root}>
+      <div className={classes.root}>
         <GridList
-          style={styles.gridList}
-          cellHeight={300}
+          className={classes.gridList}
+          cellHeight={cellHeight}
+          cols={6}
         >
           {this.renderList()}
         </GridList>
@@ -46,4 +62,8 @@ function mapStateToProps({ npcs }) {
   return { npcs };
 }
 
-export default connect(mapStateToProps)(NpcList);
+NpcList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(NpcList));
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -13,8 +12,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <MuiThemeProvider>
       <NpcList />
-    </MuiThemeProvider>
   </Provider>
   , document.querySelector('.container'));
