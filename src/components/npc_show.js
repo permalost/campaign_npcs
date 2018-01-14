982e0a5dc0e55b8de1d1fbe0ac893476d3c42da3
permalost <permalost@gmail.com>
2018-01-13 18:53:16 -0700

    Migrating to material-ui Card for showing an NPC

--- src/components/npc_show.js
+++ src/components/npc_show.js
@@ -2,7 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+import Card, {
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia
+} from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
 import { deleteNpc } from '../actions';
+
+const styles = {
+  card: {
+    maxWidth: 450
+  },
+  media: {
+    height: 600
+  },
+  img: {
+    'object-fit': 'contain'
+  }
+};
 
 class Npc extends Component {
   onDeleteClick() {
@@ -11,31 +34,42 @@ class Npc extends Component {
     this.props.history.push('/npcs');
   }
 
+  onReturnClick() {
+    this.props.history.push('/npcs');
+  }
+
   render() {
-    const { npc } = this.props;
+    const { npc, classes } = this.props;
     return (
       <div>
-        <div>
-          <img src={npc.thumbnail} alt={npc.name} width="200" />
-          <h3>{npc.name}</h3>
-          <h5>
-            <b>Associations</b>: {npc.associations}
-          </h5>
-        </div>
-        <div>
-          <p>
-            <b>Notes</b>:{npc.notes}
-          </p>
-        </div>
-        <Link to="/npcs" className="btn btn-primary">
-          Return to list
-        </Link>
-        <button
-          className="btn btn-danger pull-xs-right"
-          onClick={this.onDeleteClick.bind(this)}
-        >
-          Delete
-        </button>
+        <Card className={classes.card}>
+          <CardHeader title={npc.name} subheader={npc.associations} />
+          <CardMedia
+            className={classes.media}
+            image={npc.thumbnail}
+            title={npc.name}
+            src="img"
+          />
+          <CardContent>
+            <Typography component="p">{npc.notes}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              dense
+              color="primary"
+              onClick={this.onReturnClick.bind(this)}
+            >
+              Return to List
+            </Button>
+            <Button
+              dense
+              color="accent"
+              onClick={this.onDeleteClick.bind(this)}
+            >
+              Delete NPC
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
@@ -45,4 +79,8 @@ function mapStateToProps({ npcs }, ownProps) {
   return { npc: npcs[ownProps.match.params.name] };
 }
 
-export default connect(mapStateToProps, { deleteNpc })(Npc);
+Npc.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, { deleteNpc })(withStyles(styles)(Npc));
