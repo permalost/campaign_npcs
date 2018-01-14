f0d00f0aa6a6b26911607071645d3a03bed951ca
permalost <permalost@gmail.com>
2018-01-13 18:59:12 -0700

    Trying out material-ui Reboot

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Reboot from 'material-ui/Reboot';
 
 import reducers from './reducers';
 
@@ -13,15 +14,18 @@ import NpcNew from './components/npc_new';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/npcs/new" component={NpcNew} />
-        <Route path="/npcs/:name" component={Npc} />
-        <Route path="/npcs" component={NpcList} />
-        <Route path="/" component={NpcList} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <div>
+    <Reboot />
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/npcs/new" component={NpcNew} />
+          <Route path="/npcs/:name" component={Npc} />
+          <Route path="/npcs" component={NpcList} />
+          <Route path="/" component={NpcList} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </div>,
   document.querySelector('.container')
 );
